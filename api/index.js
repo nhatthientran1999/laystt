7ee85ec6ddd8fc4b636387c5a005a67308a38d0f
Tran Thien <skyfallbin@gmail.com>
2026-05-07 15:57:36 +0700

    Final fix: Consolidate assets and server for Vercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
-import handler from "../dist/server/server.js";
+import handler from "./server.js";
 
 export const config = {
   runtime: "nodejs",
 };
 
 export default async (req, res) => {
-  // Chuyển đổi request từ Vercel (Node.js) sang Web Standard Request
   const protocol = req.headers["x-forwarded-proto"] || "http";
   const host = req.headers["host"];
   const url = new URL(req.url, `${protocol}://${host}`);
@@ -20,7 +19,6 @@ export default async (req, res) => {
 
   const response = await handler.fetch(request);
 
-  // Chuyển đổi Web Standard Response ngược lại Vercel (Node.js)
   res.status(response.status);
   response.headers.forEach((value, key) => {
     res.setHeader(key, value);
